1c5541e4b4d628d6f874e56199726745af80f214
Srikanth Bhogi <88508220+SrikanthAshwin@users.noreply.github.com>
2021-09-01 00:25:45 +0530

    Update settings.py

--- EncrypDecryp/settings.py
+++ EncrypDecryp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django-heroku
 from pathlib import Path
 import os
 
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
