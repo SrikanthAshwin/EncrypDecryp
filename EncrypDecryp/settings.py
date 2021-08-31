5f34c167827a9118b322357d5391199ec5034973
Srikanth Bhogi <88508220+SrikanthAshwin@users.noreply.github.com>
2021-09-01 00:11:26 +0530

    Update settings.py

--- EncrypDecryp/settings.py
+++ EncrypDecryp/settings.py
@@ -125,8 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
