f05770ef800f7d8d2330562422bcd564f772175f
Srikanth Bhogi <88508220+SrikanthAshwin@users.noreply.github.com>
2021-08-31 23:29:48 +0530

    Update settings.py

--- EncrypDecryp/settings.py
+++ EncrypDecryp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=)asp7#k#p#0d!7bb)clf$6801qiv&-f61ozbuwc&jvb!4l168
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','encryptiondecryptionsrima.herokuapp.com']
 
 
 # Application definition
